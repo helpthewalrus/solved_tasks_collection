58012201a2fff64348a2d567448c3de30524cc4d
Alina Tylets <alina_tylets@epam.com>
2019-08-19 18:15:06 +0300

    feat: add styles and constatnts

---
+++ constants.js
@@ -0,0 +1,6 @@
+const constants = {
+  TOKEN: '6e72b949614b1a3b17c4f7e0cf96889b918f79ab',
+}
+
+export default constants;
+
--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,22 @@ import {
   catchError
 } from 'rxjs/operators';
 
-const TOKEN = '18b48cb8901c982a0617a5a3707a1a4e8ad70e2e';
+import constants from '../constants';
+
 const input = document.querySelector('input');
+const outputUl = document.getElementById('output');
 
 
+function createLiElement(innerText: any) {
+  const listItem = document.createElement('li');
+  listItem.innerText = innerText;
+  return listItem;
+}
 
 // Observable for fetching data from the server
 const fetchedData = (keys: any) => {
   if (keys) {
-    return fromFetch(`https://api.github.com/search/users?q=${keys}+in%3Alogin&access_token=${TOKEN}&per_page=10`).pipe(
+    return fromFetch(`https://api.github.com/search/users?q=${keys}+in%3Alogin&access_token=${constants.TOKEN}&per_page=10`).pipe(
       switchMap(response => {
         if (response.ok) {
           // OK return data
@@ -33,25 +40,24 @@ const fetchedData = (keys: any) => {
       })
     );
   } else {
-    document.getElementById('output').innerText = 'Something should be provided for search';
+    outputUl.innerText = 'Something should be provided for search';
   }
 }
-
 
 // Observable that handles search process
 fromEvent(input, 'input')
   .pipe(
-    tap(_ => document.getElementById('output').innerText = ''), // deletes already found items if user inputs smth again
+    tap(_ => outputUl.innerText = ''), // deletes already found items if user inputs smth again
     map((event: any) => event.target.value),
     debounceTime(500),
     distinctUntilChanged(),
     switchMap(fetchedData), // fetches needed data from the server
     tap((data: any) => {
       if (data.items.length === 0) {
-        document.getElementById('output').innerText = 'There is no github login with provided input data';
+        outputUl.innerText = 'There is no github login with provided input data';
       } else {
         data.items.map((item: any) => {
-          document.getElementById('output').innerText += (item.login + '\n');
+          outputUl.appendChild(createLiElement(item.login));
         })
       }
     })
